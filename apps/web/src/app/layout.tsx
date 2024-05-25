4741b236b26e1fe737ff6c16797ef4f089f81619
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-05-24 23:22:24 -0500

    add initial nextjs web app base

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = { title: 'Web App', description: 'Monorepo Web App' };
+
+type RootLayoutProps = Readonly<{ children: React.ReactNode }>;
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
