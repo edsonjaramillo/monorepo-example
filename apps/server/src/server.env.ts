db46f8c293876df6718b3018e08409b85ba17ac9
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-05-26 00:38:28 -0500

    chore: Update NODE_ENV order and add new environment variables for db

--- apps/server/src/server.env.ts
+++ apps/server/src/server.env.ts
@@ -1,10 +1,19 @@
 import { z } from 'zod';
 
-const NODE_ENV = ['development', 'test', 'staging', 'production'] as const;
+const NODE_ENV = ['development', 'production', 'staging', 'test'] as const;
+
+const zBoolean = z
+  .string()
+  .regex(/^(true|false)$/i)
+  .optional()
+  .transform(Boolean);
 
 const serverEnvSchema = z.object({
   NODE_ENV: z.enum(NODE_ENV),
   PORT: z.string().regex(/^\d+$/).transform(Number),
+  DATABASE_URL: z.string().regex(/^postgres:\/\/.*$/),
+  DB_MIGRATING: zBoolean,
+  DB_SEEDING: zBoolean,
 });
 
 export const serverEnv = serverEnvSchema.parse(process.env);
