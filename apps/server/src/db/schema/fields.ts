a0fd337117304831dd502b0399f426de42f7aa6f
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-05-26 23:48:05 -0500

    feat: Add id and timestamp fields to database schema

---
+++ apps/server/src/db/schema/fields.ts
@@ -0,0 +1,11 @@
+import { text, timestamp } from 'drizzle-orm/pg-core';
+import { uuidv7 } from 'uuidv7';
+
+export const id = {
+  id: text('id').notNull().primaryKey().$defaultFn(uuidv7),
+};
+
+export const timestampFields = {
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+} as const;
