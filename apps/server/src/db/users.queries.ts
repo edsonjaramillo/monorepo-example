17af030c673d03744dc4658f41a55b175d52789a
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-05-27 23:16:10 -0500

    feat: Refactor UsersQueries class to use default users columns

--- apps/server/src/db/users.queries.ts
+++ apps/server/src/db/users.queries.ts
@@ -1,31 +1,34 @@
 import { eq, sql } from 'drizzle-orm';
 
 import { db } from './client';
+import { DEFAULT_USERS_COLUMNS } from './columns/users.columns';
 import { usersTable } from './schema';
+import type { UserCreate } from './types/users.types';
 
-class UsersQueries {
-  async getUsers() {
-    const query = db.query.usersTable.findMany().prepare('getUsers');
-
-    return query.execute();
+export class UsersQueries {
+  static async getUsers() {
+    return await db.query.usersTable.findMany({ columns: DEFAULT_USERS_COLUMNS });
   }
 
-  async getUserById(id: string) {
+  static async getUserById(id: string) {
     const query = db.query.usersTable
-      .findFirst({ where: eq(usersTable.id, sql.placeholder('id')) })
+      .findFirst({
+        where: eq(usersTable.id, sql.placeholder('id')),
+        columns: DEFAULT_USERS_COLUMNS,
+      })
       .prepare('getUserById');
 
     return query.execute({ id });
   }
 
-  async createUser(name: string) {
+  static async createUser(user: UserCreate) {
     const query = db
       .insert(usersTable)
-      .values({ name: sql.placeholder('name') })
+      .values({
+        name: sql.placeholder('name'),
+      })
       .prepare('createUser');
 
-    return query.execute({ name });
+    return query.execute({ name: user.name });
   }
 }
-
-export const userQueries = new UsersQueries();
