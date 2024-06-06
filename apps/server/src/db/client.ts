5c6a486e8d9e5dfe2198b4730dad279d5eb6ff50
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-06 10:24:04 -0500

    chore: Refactor database connection and environment configuration

--- apps/server/src/db/client.ts
+++ apps/server/src/db/client.ts
@@ -5,11 +5,6 @@ import { serverEnv } from '../server.env';
 import * as schema from './schema';
 
 function maxPoolCountByEnviroment() {
-  if (serverEnv.DB_MIGRATING || serverEnv.DB_SEEDING) {
-    console.log('Running in migration or seeding mode, using max pool count of 1');
-    return 1;
-  }
-
   switch (serverEnv.NODE_ENV) {
     case 'development':
       return 1;
@@ -27,7 +22,6 @@ function maxPoolCountByEnviroment() {
 
 export const connection = postgres(serverEnv.DATABASE_URL, {
   max: maxPoolCountByEnviroment(),
-  onnotice: serverEnv.DB_SEEDING ? () => {} : undefined,
 });
 
 export const db = drizzle(connection, { schema });
--- apps/server/src/server.env.ts
+++ apps/server/src/server.env.ts
@@ -2,18 +2,10 @@ import { z } from 'zod';
 
 const NODE_ENV = ['development', 'production', 'staging', 'test'] as const;
 
-const zBoolean = z
-  .string()
-  .regex(/^(true|false)$/i)
-  .optional()
-  .transform((value) => value === 'true');
-
 const serverEnvSchema = z.object({
   NODE_ENV: z.enum(NODE_ENV),
   PORT: z.string().regex(/^\d+$/).transform(Number),
   DATABASE_URL: z.string().regex(/^postgres:\/\/.*$/),
-  DB_MIGRATING: zBoolean,
-  DB_SEEDING: zBoolean,
 });
 
 export const serverEnv = serverEnvSchema.parse(process.env);
