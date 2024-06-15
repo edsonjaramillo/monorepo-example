2b3aaf9a325a9cb850385050388ef24b382acf8e
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-15 12:14:26 -0500

    add ui and common components to make sure its working

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,15 @@
+import { DateTZ } from 'common/datetime';
+
+import { Text } from 'ui';
+
 export default function HomePage() {
-  return <h1>Web App</h1>;
+  const todayMidnight = DateTZ()
+    .set('hour', 0)
+    .set('minute', 0)
+    .set('second', 0)
+    .set('millisecond', 0);
+
+  const dateStr = todayMidnight.format('YYYY-MM-DD HH:mm:ss');
+
+  return <Text as="h1">{dateStr}</Text>;
 }
