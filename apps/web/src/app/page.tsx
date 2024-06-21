bba16d272851a69530e9655e5d2b05507b871df4
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-21 13:13:50 -0500

    refactor: Update fetcher in HomePage to use server fetcher utility

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,16 +1,22 @@
+import type { User } from 'db';
+
 import { DateTZ } from 'common';
 
 import { Button, Text } from 'ui';
 
-export default function HomePage() {
-  const todayMidnight = DateTZ();
-  const dateString = todayMidnight.format('YYYY-MM-DD HH:mm:ss');
+import { fetcher } from '../utils/clients';
+
+export default async function HomePage() {
+  const now = DateTZ();
+  const dateString = now.format('YYYY-MM-DD HH:mm:ss');
+  const { data } = await fetcher.get<User[]>('/users');
 
   return (
     <>
       <Text as="h1" size="cta">
         {dateString}
       </Text>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
       <Button type="button">Click me</Button>
     </>
   );
