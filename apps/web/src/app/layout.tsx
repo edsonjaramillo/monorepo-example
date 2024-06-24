d56dabdbbaad1bd96234e7e6183c38c615d177f8
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-24 18:47:06 -0500

    feat: Add Toast component to RootLayout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import { Toast } from 'ui';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -12,7 +14,10 @@ type RootLayoutProperties = Readonly<{ children: React.ReactNode }>;
 export default function RootLayout({ children }: RootLayoutProperties) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Toast />
+      </body>
     </html>
   );
 }
