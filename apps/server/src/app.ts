c4c336d9db9a38b49d1754928f42f95283310def
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-24 01:21:09 -0500

    feat: Add authentication route

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -3,6 +3,7 @@ import { cors } from 'hono/cors';
 
 import { Logger } from 'common';
 
+import { authRouter } from './routers/auth.router';
 import { imagesRouter } from './routers/images.router';
 import { usersRouter } from './routers/users.router';
 import { serverEnv } from './server.env';
@@ -12,6 +13,7 @@ const app = new Hono();
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.route('/users', usersRouter);
 app.route('/images', imagesRouter);
+app.route('/auth', authRouter);
 
 Logger.info('Server is running on port', serverEnv.PORT);
 
