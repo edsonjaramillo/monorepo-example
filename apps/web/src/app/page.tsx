33f14458e5c2e8b89b868436ede1303d0669278a
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-25 19:25:17 -0500

    refactor: Update home page component to simplify

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,23 +1,16 @@
-import Link from 'next/link';
-
 import { DateTZ } from 'common';
 
-import { Text } from 'ui';
+import { Responsive, Text } from 'ui';
 
 export default async function HomePage() {
   const now = DateTZ();
   const dateString = now.format('YYYY-MM-DD HH:mm:ss');
 
   return (
-    <>
+    <Responsive>
       <Text as="h1" size="cta">
         {dateString}
       </Text>
-
-      <div className="flex gap-4">
-        <Link href="/signin">Sign in</Link>
-        <Link href="/signup">Sign up</Link>
-      </div>
-    </>
+    </Responsive>
   );
 }
