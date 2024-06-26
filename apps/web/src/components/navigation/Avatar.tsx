409491a776bde10575dc293d836fb8ef429f9744
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-25 19:25:53 -0500

    refactor: Update Avatar component to support signed-in and signed-out states

--- apps/web/src/components/navigation/Avatar.tsx
+++ apps/web/src/components/navigation/Avatar.tsx
@@ -1,20 +1,51 @@
 'use client';
 
-import Link from 'next/link';
+import { UserCircleIcon } from '@heroicons/react/24/solid';
 
+import { useAuthPopup } from '../../lib/authPopup.context';
 import { useSession } from '../../lib/session.context';
+import { AuthPopup } from '../AuthDropdown';
 
 export function Avatar() {
   const { session } = useSession();
-  if (!session) {
-    return <Link href="/signin">Sign In</Link>;
-  }
-
-  const firstLetter = session.user.name[0].toUpperCase() || '?';
 
   return (
-    <div className="flex size-8 items-center justify-center rounded-full bg-primary-600 font-bold text-grayscale-50">
-      <span>{firstLetter}</span>
+    <div className="relative">
+      {session ? <SignedInAvatar /> : <SignedOutAvatar />}
+      <AuthPopup />
     </div>
+  );
+}
+
+function SignedInAvatar() {
+  const { session } = useSession();
+  const { isOpen, setAuthPopup } = useAuthPopup();
+
+  const firstLetter = session!.user.name[0].toUpperCase();
+
+  return (
+    <button
+      type="button"
+      className="flex size-8 items-center justify-center rounded-full bg-primary-600 font-bold text-grayscale-50"
+      onClick={() => {
+        setAuthPopup(!isOpen);
+      }}>
+      <span>{firstLetter}</span>
+    </button>
+  );
+}
+
+function SignedOutAvatar() {
+  const { isOpen, setAuthPopup } = useAuthPopup();
+  return (
+    <button
+      type="button"
+      className="flex items-center justify-center rounded-full font-bold text-grayscale-50"
+      onClick={() => {
+        setAuthPopup(!isOpen);
+      }}>
+      <span className="sr-only">Signed Out Avatar</span>
+      <UserCircleIcon className="size-8 text-grayscale-950" />
+    </button>
   );
 }
