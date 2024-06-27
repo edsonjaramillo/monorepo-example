d998fd8e824c268ba5c9aaaf526b893fe93da0ac
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-27 12:23:40 -0500

    refactor: Update zUploadImageFormSchema to allow any type for image

--- packages/validation/src/forms/images.validation.ts
+++ packages/validation/src/forms/images.validation.ts
@@ -4,7 +4,8 @@ import { zString } from '../core';
 
 export const zUploadImageFormSchema = z.object({
   folder: zString,
-  image: z.instanceof(File),
+  // image: z.instanceof(File),
+  image: z.any(),
 });
 
 export type UploadImageForm = z.infer<typeof zUploadImageFormSchema>;
