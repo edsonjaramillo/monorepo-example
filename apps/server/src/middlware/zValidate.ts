c7d10601b6f13718f4e3d0e1d41ce35a4f482215
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-27 15:53:24 -0500

    refactor: Update middleware for request validation

---
+++ apps/server/src/middlware/zValidate.ts
@@ -0,0 +1,46 @@
+import type { Env, Input, MiddlewareHandler, ValidationTargets } from 'hono';
+import { validator } from 'hono/validator';
+import { type ZodSchema, type z } from 'zod';
+
+type HasUndefined<T> = undefined extends T ? true : false;
+
+export const zValidator = <
+  Target extends keyof ValidationTargets,
+  T extends ZodSchema,
+  E extends Env,
+  P extends string,
+  In = z.input<T>,
+  Out = z.output<T>,
+  I extends Input = {
+    in: HasUndefined<In> extends true
+      ? {
+          [K in Target]?: K extends 'json'
+            ? In
+            : HasUndefined<keyof ValidationTargets[K]> extends true
+              ? { [K2 in keyof In]?: ValidationTargets[K][K2] }
+              : { [K2 in keyof In]: ValidationTargets[K][K2] };
+        }
+      : {
+          [K in Target]: K extends 'json'
+            ? In
+            : HasUndefined<keyof ValidationTargets[K]> extends true
+              ? { [K2 in keyof In]?: ValidationTargets[K][K2] }
+              : { [K2 in keyof In]: ValidationTargets[K][K2] };
+        };
+    out: { [K in Target]: Out };
+  },
+  V extends I = I,
+>(
+  schema: T,
+  target: Target,
+): MiddlewareHandler<E, P, V> =>
+  // @ts-expect-error not typed well
+  validator(target, async (value) => {
+    const result = await schema.safeParseAsync(value);
+
+    if (!result.success) {
+      throw new Error(`Invalid ${target}.`);
+    }
+
+    return result.data as z.infer<T>;
+  });
