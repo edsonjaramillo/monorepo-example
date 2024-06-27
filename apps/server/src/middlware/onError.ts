242ea1362b5a6da62395508cebc46b4d150018fe
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-26 22:32:17 -0500

    refactor: Improve error handling and logging in onError middleware

--- apps/server/src/middlware/onError.ts
+++ apps/server/src/middlware/onError.ts
@@ -2,8 +2,14 @@ import type { ErrorHandler } from 'hono';
 
 import { JSend } from 'common';
 
+import { serverEnv } from '../server.env';
+
 export function onError(): ErrorHandler {
   return function (error, c) {
+    if (serverEnv.NODE_ENV === 'development') {
+      console.error(error);
+    }
+
     return c.json(JSend.error(error.message), 500);
   };
 }
