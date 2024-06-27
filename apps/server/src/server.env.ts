c400a6b873fbad6eb81e8d6a7c35ac2b8bd3f1cb
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-06-26 22:32:45 -0500

    refactor: Update server.env.ts to use zString from validation package and add S3 configuration options

--- apps/server/src/server.env.ts
+++ apps/server/src/server.env.ts
@@ -1,16 +1,18 @@
 import { z } from 'zod';
 
-import { NODE_ENV } from 'validation';
+import { zString } from 'validation';
+
+import { NODE_ENV } from 'common';
 
 const serverEnvSchema = z.object({
   NODE_ENV: z.enum(NODE_ENV),
-  PORT: z.string().regex(/^\d+$/).transform(Number),
-  DATABASE_URL: z.string().regex(/^postgres:\/\/.*$/),
-  BACKBLAZE_ENDPOINT: z.string().trim(),
-  BACKBLAZE_REGION: z.string().trim(),
-  BACKBLAZE_ACCOUNT_ID: z.string().trim(),
-  BACKBLAZE_APPLICATION_KEY: z.string().trim(),
-  BACKBLAZE_BUCKET_NAME: z.string().trim(),
+  PORT: zString.regex(/^\d+$/).transform(Number),
+  DATABASE_URL: zString.regex(/^postgres:\/\/.*$/),
+  S3_ENDPOINT: zString,
+  S3_REGION: zString,
+  S3_ACCESS_KEY_ID: zString,
+  S3_SECRET_ACCESS_KEY_ID: zString,
+  S3_BUCKET_NAME: zString,
 });
 
 export const serverEnv = serverEnvSchema.parse(process.env);
