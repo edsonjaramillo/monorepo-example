332a7cc9dae32214847e549e6885a38db6358734
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-07-02 13:21:39 -0500

    feat: Add StringUtils module for string manipulation

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -3,3 +3,4 @@ export * from './env';
 export * from './fetching';
 export * from './http';
 export * from './logging';
+export * from './utils';
---
+++ packages/common/src/utils/String.utils.ts
@@ -0,0 +1,5 @@
+export const StringUtils = {
+  capitalize(string_: string): string {
+    return string_.charAt(0).toUpperCase() + string_.slice(1);
+  },
+};
