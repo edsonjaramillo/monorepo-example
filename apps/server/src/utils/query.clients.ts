f916f29ba1bca21fd49740333ca913280b63804e
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-07-04 23:22:29 -0500

    refactor: Update import paths for new filenames

--- apps/server/src/utils/query.clients.ts
+++ apps/server/src/utils/query.clients.ts
@@ -1,6 +1,6 @@
 import { ImagesQueries, SessionsQueries, UsersQueries } from 'db';
 
-import { cache, database } from '../db';
+import { cache, database } from '../server.clients';
 
 export const imagesQueries = new ImagesQueries(database, cache);
 export const sessionsQueries = new SessionsQueries(database, cache);
