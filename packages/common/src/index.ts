9779670612900a2bda70143f72a93accaf975ab1
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-07-05 14:16:01 -0500

    refactor: move to node_env common constant into shared.ts

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,5 +1,4 @@
 export * from './datetime';
-export * from './env';
 export * from './fetching';
 export * from './http';
 export * from './logging';
--- packages/common/src/shared.ts
+++ packages/common/src/shared.ts
@@ -1,2 +1,5 @@
+export const NODE_ENV = ['development', 'production', 'staging', 'test'] as const;
+export type NodeEnv = (typeof NODE_ENV)[number];
+
 export const folders = ['users', 'misc'] as const;
 export type Folders = (typeof folders)[number];
