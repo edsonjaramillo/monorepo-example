3e93850ed9473c7dce63d846f4e2f61a952f4bce
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-07-04 21:36:44 -0500

    refactor: Update users router to include admin and self routes

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -9,7 +9,7 @@ import { sessionify } from './middlware/sessionify';
 import { publicAuthRouter, userAuthRouter } from './routers/auth.router';
 import { debugRouter } from './routers/debug.router';
 import { employeeImagesRouter } from './routers/images.router';
-import { usersRouter } from './routers/users.router';
+import { adminUsersRouter, usersSelfRouter } from './routers/users.router';
 import { serverEnv } from './server.env';
 
 const app = new Hono();
@@ -20,16 +20,23 @@ if (serverEnv.NODE_ENV === 'development') {
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
+// session verification middleware
 app.use('/user/*', sessionify(['admin', 'employee', 'user']));
 app.use('/employee/*', sessionify(['admin', 'employee']));
 app.use('/admin/*', sessionify(['admin']));
 
-app.route('/users', usersRouter);
+// user routers
+app.route('/admin/users', adminUsersRouter);
+app.route('/user/self', usersSelfRouter);
+
+// images router
 app.route('/employee/images', employeeImagesRouter);
 
+// auth routers
 app.route('/auth', publicAuthRouter);
 app.route('/user/auth', userAuthRouter);
 
+// debug router
 app.route('/debug', debugRouter);
 
 app.onError(onError());
--- apps/server/src/routers/users.router.ts
+++ apps/server/src/routers/users.router.ts
@@ -1,13 +1,16 @@
 import { Hono } from 'hono';
 
+import { zSelfUpdateUserSchema } from 'validation';
+
 import { JSend, calculatePaginationDetails } from 'common';
 
 import { paginate } from '../middlware/paginate';
+import { zValidator } from '../middlware/zValidate';
 import { usersQueries } from '../utils/query.clients';
 
-export const usersRouter = new Hono();
+export const adminUsersRouter = new Hono();
 
-usersRouter.get('/', paginate(), async (c) => {
+adminUsersRouter.get('/', paginate(), async (c) => {
   const { limit, offset, page } = c.get('pagination');
 
   const users = await usersQueries.getUsers(limit, offset);
@@ -17,14 +20,24 @@ usersRouter.get('/', paginate(), async (c) => {
   return c.json(JSend.pagination(users, pagination, 'Users fetched successfully'));
 });
 
-usersRouter.get('/:id', async (c) => {
-  const { id } = c.req.param();
+export const usersSelfRouter = new Hono();
 
-  const user = await usersQueries.getUserById(id);
+usersSelfRouter.get('/', async (c) => {
+  const session = c.get('session');
 
+  const user = await usersQueries.getUserById(session.user.id);
   if (!user) {
     return c.json(JSend.error('User not found'), 404);
   }
 
   return c.json(JSend.success(user, 'User fetched successfully'));
+});
+
+usersSelfRouter.put('/update', zValidator(zSelfUpdateUserSchema, 'json'), async (c) => {
+  const body = c.req.valid('json');
+  const session = c.get('session');
+
+  await usersQueries.selfUpdateUser(session.user.id, body);
+
+  return c.json(JSend.success(undefined, 'User updated successfully'));
 });
