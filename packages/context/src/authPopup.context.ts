bd057b6df2baf368b364c6ed13adbbc7f1557b88
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-07-05 09:43:14 -0500

    refactor: Update authPopup.context.ts to use boolean type for isOpen and remove undefined

--- packages/context/src/authPopup.context.ts
+++ packages/context/src/authPopup.context.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-type State = { isOpen: boolean | undefined };
+type State = { isOpen: boolean };
 type Actions = {
   setAuthPopup: (status: boolean) => void;
 };
