b4cd750115b9eeac548f8c63a17960ce8b26fa10
Edson Jaramillo <edsonajaramillo@gmail.com>
2024-07-05 22:49:13 -0500

    refactor: Update web.clients.ts to use NEXT_PUBLIC_SERVER_URL instead of SERVER_URL

--- apps/web/src/utils/web.clients.ts
+++ apps/web/src/utils/web.clients.ts
@@ -2,5 +2,5 @@ import { ClientFetcher, ServerFetcher } from 'common';
 
 import { webEnv } from '../web.env';
 
-export const clientFetcher = new ClientFetcher(webEnv.SERVER_URL);
-export const serverFetcher = new ServerFetcher(webEnv.SERVER_URL);
+export const clientFetcher = new ClientFetcher(webEnv.NEXT_PUBLIC_SERVER_URL);
+export const serverFetcher = new ServerFetcher(webEnv.NEXT_PUBLIC_SERVER_URL);
